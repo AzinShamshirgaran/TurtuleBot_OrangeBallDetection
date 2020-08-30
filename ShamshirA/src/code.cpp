726b25a3d9b7dfc4ea75b11a213dc25dae4b423b
Azin Shamshirgaran <36013491+azinsh1988@users.noreply.github.com>
2020-08-29 20:07:59 -0400

    Add files via upload

---
+++ ShamshirA/src/code.cpp
@@ -0,0 +1,197 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+#include <geometry_msgs/Twist.h>
+
+
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+  image_transport::Subscriber depth_sub_;
+
+  ros::Publisher velocity_publisher =nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 10);
+
+  int ball_row, ball_col;
+  int noOfRow, noOfCol;
+  float linearVel;
+  
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/usb_cam/image_raw", 10, &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 10);
+    depth_sub_ = it_.subscribe("/camera/depth/image_raw", 10, &ImageConverter::getDist, this);
+
+
+
+
+
+    cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+
+    int row = 0; 
+    int col = 0; 
+    int dis_min = 1000000; 
+    for (int i=0; i < cv_ptr->image.rows; i++) {
+        for (int j=0; j < cv_ptr->image.cols; j++) {
+            int b = cv_ptr->image.at<cv::Vec3b>(i, j).val[0];
+            int g = cv_ptr->image.at<cv::Vec3b>(i, j).val[1];
+            int r = cv_ptr->image.at<cv::Vec3b>(i, j).val[2]; 
+            // int dis = pow((r-255)*(r-255) + g*g + b*b, 0.5); 
+            int dis = (r-255)*(r-255) + g*g + b*b; 
+            // ROS_INFO("b: %d", b); 
+            // ROS_INFO("g: %d", g); 
+            // ROS_INFO("r: %d", r); 
+            // ROS_INFO("%d", dis); 
+            if (dis < dis_min) {
+                dis_min = dis; 
+                row = i; 
+                col = j; 
+            }
+        }
+    }
+    noOfRow=msg->height;
+    noOfCol=msg->width;
+
+    ROS_INFO("NoOfRow: %d", msg->height);
+    ROS_INFO("NoOfCol: %d", msg->width);
+//NoOfRow=480 & NooCol=640
+    ball_row = row;
+    ball_col = col;
+
+
+    // ROS_INFO("value: %d", cv_ptr->image.at<cv::Vec3b>(0,0).val[0]); 
+    // ROS_INFO("value: %d", cv_ptr->image.at<cv::Vec3b>(0,0).val[1]); 
+    // ROS_INFO("value: %d", cv_ptr->image.at<cv::Vec3b>(0,0).val[2]); 
+    // ROS_INFO("height: %d", msg->height); 
+    // ROS_INFO("width: %d", msg->width); 
+    // ROS_INFO_STREAM("encoding: " << msg->encoding); 
+
+   
+    ROS_INFO("r: %d", row); 
+    ROS_INFO("c: %d", col); 
+
+    cv::circle(cv_ptr->image, cv::Point(col, row), 10, CV_RGB(255,0,0));
+
+    // Update GUI Window
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(3);
+    
+    // Output modified video stream
+    image_pub_.publish(cv_ptr->toImageMsg());
+  }
+
+    void getDist(const sensor_msgs::Image::ConstPtr& msg) //in this method we get the distance to the ball
+  {
+      //get dist to ball
+      cv_bridge::CvImagePtr cv_ptr;
+      try
+      {
+          cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
+          float distance = cv_ptr->image.at<float>(ball_row, ball_col);
+          ROS_INFO("Distance: %f", distance);
+
+          if (distance < 950 && distance>0)         //1000 is equivalent to 1 m
+              linearVel =-0.2;
+          else if (distance > 1100)
+              linearVel = 0.2;
+          else if(distance ==0.00)
+              linearVel=linearVel       ;
+       
+      }
+      catch (cv_bridge::Exception& e)
+      {
+          ROS_ERROR("cv_bridge exception: %s", e.what());
+      }
+  }
+
+
+
+    void setCurrentGoalAndPublishVel() //in this method we set the linear and angular vel to the ball
+    {
+
+      
+      geometry_msgs::Twist vel_msg;
+
+      vel_msg.linear.y =0;
+      vel_msg.linear.z =0;
+      
+      vel_msg.angular.y = 0;
+      vel_msg.angular.x =0;
+
+      if ( ball_col > (0.55*noOfCol)) 
+      {
+         vel_msg.linear.x =linearVel;
+         vel_msg.angular.z = -2*(ball_col-(320))/320;
+      }
+       
+      else if (ball_col < (0.45*noOfCol))
+      {
+         vel_msg.linear.x =linearVel;
+         vel_msg.angular.z = -2*(ball_col-(320))/320; 
+      } 
+      else
+      {
+         vel_msg.linear.x =linearVel;
+         vel_msg.angular.z = 0.1; 
+      } 
+      ROS_INFO("Angulvel: %f", vel_msg.angular.x );
+      velocity_publisher.publish(vel_msg);
+
+    }
+
+
+
+};
+
+int main(int argc, char** argv)
+{
+  
+  ros::init(argc, argv, "image_converter");
+ 
+  ImageConverter ic;
+ 
+
+
+  while(ros::ok())
+  {
+    ros::spinOnce();
+
+    ic.setCurrentGoalAndPublishVel();
+
+  }
+  ros::spin();
+
+  return 0;
+}
